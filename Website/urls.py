41922764e732f497198f7061930655a92f4a30ed
Ronakjain515 <Jain@810Jain@810>
2021-01-07 00:04:25 +0530

    Add login register UI pages

--- Website/urls.py
+++ Website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Index"),
+    path('Login', views.login, name="Login"),
+    path('Register', views.register, name="Register"),
 ]
